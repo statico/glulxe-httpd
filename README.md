369fc9f0c53c11a2566f85ced37991cf6f358e6c
Ian Langworth ☠ <ian@langworth.com>
2020-11-20 22:01:36 -0800

    Convert server to typescript, use child process

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # glulxe-httpd
+
 HTTP+JSON .ulx interactive fiction server with session support
+
+## See Also
+
+These projects are good but don't support sessions and/or don't save transcripts:
+
+- https://github.com/agentcox/glknode
+- https://github.com/erkyrath/remote-if-demo
+
+My original version of this used a Z-machine emulator for .z8 games: https://github.com/statico/ifhttp
---
+++ server.ts
@@ -0,0 +1,192 @@
+import * as bodyParser from 'body-parser'
+import { ChildProcess, spawn } from 'child_process'
+import commander from 'commander'
+import * as cors from 'cors'
+import * as express from 'express'
+import * as fs from 'fs'
+import { fileSize } from 'humanize-plus'
+import { unparse } from 'papaparse'
+import touch from 'touch'
+import uuid from 'uuid'
+
+require('console-stamp')(console)
+
+const sleep = (ms: number) =>
+  new Promise((resolve, reject) => {
+    setTimeout(resolve, ms)
+  })
+
+commander
+  .usage('<story.z8>')
+  .option('-d, --debug', 'Always create/return the same session ID, "test"')
+  .option(
+    '-t, --session-timeout <timeout>',
+    'Session timeout (in seconds)',
+    '900'
+  )
+  .option('-c, --csv <path>', 'Log game sessions to a CSV file')
+  .option('-p, --port <port>', 'Port to bind to', '8080')
+  .parse(process.argv)
+
+if (commander.args.length !== 1) {
+  commander.help()
+}
+
+const sessions: { [key: string]: Session } = {}
+
+setInterval(function () {
+  const t = Date.now() - Number(commander.sessionTimeout) * 1000
+
+  // Cleanup idle sessions.
+  for (const id in sessions) {
+    const sess = sessions[id]
+    if (sess.lastUpdate < t) {
+      console.log('Deleted session', id)
+      sess.close()
+      delete sessions[id]
+    }
+  }
+
+  // Print memory usage to console.
+  const mem = process.memoryUsage()
+  for (const k in mem) {
+    const v = mem[k]
+    mem[k] = fileSize(v)
+  }
+  return console.log(
+    `${Object.keys(sessions).length} sessions, memory: ${JSON.stringify(mem)}`
+  )
+}, 60 * 1000)
+
+class Session {
+  public id: string
+  public running: boolean
+  public lastUpdate: number
+  private process: ChildProcess
+  private buffer: string
+
+  constructor() {
+    this.id = commander.debug ? 'test' : uuid.v4()
+    this.running = true
+    this.lastUpdate = Date.now()
+    this.buffer = ''
+
+    this.process = spawn('glulxe', [commander.args[0]])
+    this.process.stdout.on('data', (data) => {
+      this.buffer += data
+    })
+    this.process.on('exit', (code, signal) => {
+      console.log(
+        `Session ${this.id} exited with signal ${signal} and code ${code}`
+      )
+      this.running = false
+    })
+    this.process.on('error', (err) => {
+      console.log(`Session ${this.id} error: ${err}`)
+
+      this.running = false
+    })
+  }
+
+  close() {
+    this.process.kill()
+    this.running = false
+  }
+
+  getBuffer(): string {
+    const output = this.buffer.replace(/^\s+/, '') // Trim leading space.
+    this.buffer = ''
+    return output
+  }
+
+  async send(input: string): Promise<string> {
+    if (!this.running) {
+      throw new Error('Interpreter not running')
+    }
+    this.lastUpdate = Date.now()
+    this.process.stdin.write(input.trim() + '\n')
+    await sleep(100)
+    return this.getBuffer().trim()
+  }
+}
+
+function logToCSV(
+  addr: string,
+  sessionId: string,
+  message: string,
+  reply: string
+) {
+  if (!commander.csv) return
+  const datetime = new Date().toISOString().slice(0, 19).replace('T', ' ')
+  const line = unparse([[datetime, sessionId, addr, message, reply]])
+  try {
+    fs.appendFileSync(commander.csv, line, 'utf8')
+  } catch (err) {
+    console.error(`Could not write to ${commander.csv}:`, err)
+  }
+}
+
+if (commander.csv) {
+  touch.sync(commander.csv)
+  console.log(`Logging sessions as CSV to ${commander.csv}`)
+}
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/', function (req, res) {
+  res.set('Content-Type', 'text/plain')
+  res.send('ok\n')
+})
+
+app.get('/new', async function (req, res) {
+  const remoteAddr = req.get('x-forwarded-for') || req.connection.remoteAddress
+  const sess = new Session()
+  sessions[sess.id] = sess
+  console.log(sess.id, remoteAddr, '(new session)')
+  await sleep(100)
+  res.json({ session: sess.id, output: sess.getBuffer() })
+})
+
+app.post('/send', async function (req, res) {
+  const remoteAddr = req.get('x-forwarded-for') || req.connection.remoteAddress
+
+  // Simple input sanitization.
+  const message = req.body.message?.substr(0, 255).replace(/[^\w ]+/g, '')
+
+  const { session } = req.body
+  if (session == null || message == null) {
+    res.status(400).json({ error: 'Missing session or message' })
+    return
+  }
+
+  const sess = sessions[session]
+  if (sess == null) {
+    res.status(400).json({ error: 'No such session' })
+    return
+  }
+
+  try {
+    console.log(sess.id, remoteAddr, JSON.stringify(message))
+    const output = await sess.send(message)
+    logToCSV(remoteAddr, sess.id, message, output)
+    res.json({ output })
+    if (!sess.running) {
+      delete sessions[session]
+    }
+  } catch (err) {
+    console.error(sess.id, remoteAddr, `Error: ${err}`)
+    res.status(500).json({ error: err })
+    return
+  }
+})
+
+if (commander.debug) {
+  // Skip an extra request when debugging.
+  sessions['test'] = new Session()
+}
+
+const listener = app.listen(Number(commander.port), () => {
+  console.log(`ifhttp listening at http://localhost:${commander.port}`)
+})
